8c0558e807df5336219f6f00f713b2976a5f550d
Swarup Dash <swarupdash7205@gmail.com>
2024-10-02 23:25:11 +0530

    Initial Commit

--- src/main/java/org/example/TestDropDown.java
+++ src/main/java/org/example/TestDropDown.java
@@ -10,21 +10,37 @@ public class TestDropDown {
 	
 	public static WebDriver driver =Drivermanager.getDriver();
 	
-	public static void dropDownTest(WebElement dropdownID, String option) {
+	public static void dropDownTest(WebElement dropdown, String option) {
 //		WebElement dropdown = driver.findElement(By.id(dropdownId));
 //		dropdown.click();
 //		
 //		WebElement dropdownOption = driver.findElement(By.xpath(option));
 //		dropdownOption.click();
 		
-		
-		List<WebElement> options = driver.findElements(By.id(dropdownId));
-		for(WebElement choise:options) {
-			if(choise.getText().equals("option")) {
-				choise.click();
-				break;
+		try {
+			if (Wait.visibilityOfElement(dropdown, 10)){
+				dropdown.click();
+				List<WebElement> dopdown_value = Drivermanager.getDriver().findElements((By) dropdown);
+				for(WebElement ele:dopdown_value) {
+					if(ele.getText().contains(option)) {
+						ele.click();
+					}
+				}
 			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
 		}
+		
+		
+		
+//		List<WebElement> options = driver.findElements(By.id(dropdownId));
+//		for(WebElement choise:options) {
+//			if(choise.getText().equals("option")) {
+//				choise.click();
+//				break;
+//			}
+//		}
 		
 	}
 	
